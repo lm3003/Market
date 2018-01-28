7f27b8dfe7b15cdc7fe488414d4d60f3d06b4861
lm3003 <love.modi3003@gmail.com>
2018-01-28 13:57:09 -0500

    modifying to unzipped version with some changes

---
+++ Item.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+
+//Honor Pledge:
+//
+//I pledge that I have neither given nor 
+//received any help on this assignment.
+//
+//lmodi
+
+public class Item {
+
+    /**
+     * Default constructor
+     */
+    public Item() {
+    }
+
+    /**
+     * 
+     */
+    int id;
+
+    /**
+     * 
+     */
+    String name;
+
+    /**
+     * 
+     */
+    String description;
+
+    /**
+     * 
+     */
+    String type;
+
+    /**
+     * 
+     */
+    float price;
+
+    /**
+     * 
+     */
+    int quantity;
+
+
+
+
+
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+JCC = javac
+	
+compile:
+	$(JCC) *.java
+	
+clean:
+	$(RM) *.class
